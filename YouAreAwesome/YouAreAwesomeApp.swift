25935a17fc6d3a754b27fc6eb408f7b2f2599444
Sankar Ram Subramanian <sankarram2002ybis@gmail.com>
2026-01-13 15:35:12 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Sankar Ram Subramanian on 13/1/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
